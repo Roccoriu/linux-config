ff43dc35d45d1f4d78cbf080c79b1923a2886cfd
Rocco Ciccone <rocco.ciccone@protonmail.com>
2021-11-29 23:49:07 +0100

    added config files

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#---------------------------------------
+#-------- Include config files ---------
+#---------------------------------------
+
+# variables file
+source ~/.config/fish/conf.d/20-env_vars.conf 
+
+# alias file
+source ~/.config/fish/conf.d/10-alias.conf
+
+# general settings
+source ~/.config/fish/conf.d/30-settings.conf
+
